499be1ddb97262b24912b69dc52b35025c56d5ef
Joshua Payes <118050188+jpayes@users.noreply.github.com>
2024-12-13 09:52:57 -0800

    Update IOT_SmartLock.ino

--- IOT_SmartLock.ino
+++ IOT_SmartLock.ino
@@ -11,11 +11,11 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-char ssid[50] = " ";
-char pass[50] = " ";
+char ssid[50] = "CHANGE";
+char pass[50] = "CHANGE";
 
-const char kHostname[] = "13.56.253.10:5000";
-const char kPath[] = " ";
+const char kHostname[] = "CHANGE TO YOUR AWS IP";
+const char kPath[] = "CHANGE TO YOUR PATH";
 const int kNetworkTimeout = 30 * 1000;    // Number of milliseconds to wait if no data is available before trying again
 const int kNetworkDelay = 1000;
 
@@ -28,9 +28,6 @@ bool validUID(byte * scannedTag, byte authorizedTags[][4] , int numRows)
   // loop through valid uid array
   // if match, return true
   // else, return false
-
-
-  
   for (int row = 0; row < numRows; row++ )
   {
       for (int columns = 0; columns < 4; columns++)
@@ -53,9 +50,6 @@ bool validUID(byte * scannedTag, byte authorizedTags[][4] , int numRows)
   return false;
 }
 
-
-
-
 void setup() {
     Serial.begin(9600); // Initialize serial communications with the PC
     while (!Serial);    // Do nothing if no serial port is opened
@@ -76,17 +70,17 @@ void setup() {
       // Set LED pins as output
     pinMode(GREEN_LED_PIN, OUTPUT);
     pinMode(RED_LED_PIN, OUTPUT);
-
-    
    
 }
 
 void loop() {
+    int err = 0;
+    WiFiClient c;
+    HttpClient http(c);
     // Check if a new card is present
     if (!mfrc522.PICC_IsNewCardPresent()) {
         return;
     }
-
     // Select one of the cards
     if (!mfrc522.PICC_ReadCardSerial()) {
         return;
@@ -95,9 +89,15 @@ void loop() {
      // Calculate the number of rows in the authorizedTags array
     int numRows = sizeof(authorizedUIDs) / sizeof(authorizedUIDs[0]);
 
+    valid = validUID(mfrc522.uid.uidByte, authorizedUIDs, numRows);
+
+    // String to store on AWS web page
+    String val = String ("?authorized=") + String(valid) + String("&time=") + String(time) + String("&uid=") + String(mfrc.522.uid.uid.byte);
+    err = http.get("CHANGE TO IP ADDRESS AWS", 5000, val.c_str(), NULL);
      // Check if the scanned UID is valid
-    if (validUID(mfrc522.uid.uidByte, authorizedUIDs, numRows))
+    if (valid)
     {
+        
         Serial.println("Authorized");
         digitalWrite(GREEN_LED_PIN, HIGH);  // Turn on green LED
         delay(2000);  // Keep LED on for 2 seconds 
